137ec07504539bad0e49913ad01344695305792b
Ann-Anyango <annmwangombe@gmail.com>
2023-04-15 19:00:05 +0300

    first commit

---
+++ practice/control_flow.py
@@ -0,0 +1,31 @@
+temprature=15
+# boolen for if it's true or fale 
+if temprature >30:
+    print("it's  warm")
+    
+elif temprature >20:
+    print("go to beach")
+else:
+     print ("the weatheris not well") 
+print("done")
+age =10
+if age >=15:
+    message="Child"
+else:
+    message="adults"
+message="Child" if age >= 15 else "adults"       
+print(message)
+#logical operaters
+#and ,or,not 
+high=False
+credit=True  
+student=True
+if not student:
+    print("green")
+# if high or credit:
+    print("green")
+else: 
+
+    print("yellow")      
+
+
